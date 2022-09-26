6c99263272395e36bd9b7c7439dfa7876972a61f
gandhihernandez <gandhi.hernandez@gmail.com>
2022-09-26 13:41:02 -0500

    Add files via upload

---
+++ src/keysInterviews.js
@@ -0,0 +1,14 @@
+
+var host = 'db'
+var user = 'pablohoney'
+var password = 'Pikachuy1-'
+var database = 'AustenRiggs'
+
+module.exports = {
+    database: {
+        host: host,
+        user: user,
+        password: password,
+        database: database,
+    }
+};
